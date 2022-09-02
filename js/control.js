e2903ccb8100518361012b37aabab787413d89df
Banyapon Poolsawas <daydevthailand@gmail.com>
2022-09-02 15:30:22 +0700

    Update Gesture

--- js/control.js
+++ js/control.js
@@ -1,23 +1,24 @@
-var rotate;
-var el;
 
 AFRAME.registerComponent('rotate-box', {
     init: function () {
+        const el = document.querySelector('a-box');
+        var rotate;
         var myElement = document.getElementById('myElement');
         var mc = new Hammer(myElement);
+        var value = 0;
+        var pos_x = 0;
         mc.on("panleft panright tap press", function (ev) {
             myElement.textContent = ev.type + " gesture detected.";
-            console.log(ev.type + " gesture detected.");
             rotate = ev.type;
             console.log("Rotate: " + rotate);
-        });
-        this.el.addEventListener("click", e => {
-            if(rotate == "panleft"){
-                this.el.setAttribute("rotation", "-45 0 0")
-            }else if(rotate == "panright"){
-                this.el.setAttribute("rotation", "45 0 0")
+            if (rotate == "panleft") {
+                console.log("Left")
+                value = value - 1
+            } else if (rotate == "panright") {
+                console.log("Right")
+                value = value + 1
             }
-            
-        })
+            el.setAttribute('rotation', { x: 0, y: (pos_x+value), z: -3 });
+        });
     }
 });
