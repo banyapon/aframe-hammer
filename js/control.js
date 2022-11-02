8d4c500a32565163273050cbe74d4e8c74969ef2
Banyapon Poolsawas <daydevthailand@gmail.com>
2022-11-02 15:03:20 +0700

    Update control.js
    แก้ไขส่วนของ scale และ rotate ใหม่

--- js/control.js
+++ js/control.js
@@ -1,24 +1,54 @@
-
 AFRAME.registerComponent('rotate-box', {
     init: function () {
-        const el = document.querySelector('a-box');
-        var rotate;
+        const el = document.querySelector('#box');
         var myElement = document.getElementById('myElement');
         var mc = new Hammer(myElement);
-        var value = 0;
-        var pos_x = 0;
-        mc.on("panleft panright tap press", function (ev) {
+        mc.on('pan', (ev) => {
+            let rotation = el.getAttribute("rotation");
             myElement.textContent = ev.type + " gesture detected.";
             rotate = ev.type;
             console.log("Rotate: " + rotate);
-            if (rotate == "panleft") {
-                console.log("Left")
-                value = value - 1
-            } else if (rotate == "panright") {
-                console.log("Right")
-                value = value + 1
+
+            switch (ev.direction) {
+                case 2:
+                    rotation.y = rotation.y + 10;
+                    break;
+                case 4:
+                    rotation.y = rotation.y - 10;
+                    break;
+                case 8:
+                    rotation.x = rotation.x + 10;
+                    break;
+                case 16:
+                    rotation.x = rotation.x - 10;
+                    break;
+                default:
+                    break;
             }
-            el.setAttribute('rotation', { x: 0, y: (pos_x+value), z: -3 });
+            el.setAttribute("rotation", rotation)
         });
+
+        mc.on("pinch", (ev) => {
+            let scale = { x: ev.scale, y: ev.scale, z: ev.scale }
+            el.setAttribute("scale", scale);
+        });
+    }
+});
+
+
+AFRAME.registerComponent('ortho', {
+    init: function () {
+        var sceneEl = this.el.sceneEl;
+        sceneEl.addEventListener('render-target-loaded', () => {
+            this.originalCamera = sceneEl.camera;
+            this.cameraParent = sceneEl.camera.parent;
+            this.orthoCamera = new THREE.OrthographicCamera(-1, 1, 1, -1);
+            this.cameraParent.add(this.orthoCamera);
+            sceneEl.camera = this.orthoCamera;
+        });
+    },
+    remove: function () {
+        this.cameraParent.remove(this.orthoCamera);
+        sceneEl.camera = this.originalCamera;
     }
 });
